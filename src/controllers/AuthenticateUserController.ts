55872f1b7c4497fa9b3a364f29385bef6e69b5ad
hugolfsimoes <hugolfsimoes@gmail.com>
2022-02-13 14:02:16 -0300

    iniciando o projeto da rocket seat node heat

--- src/controllers/AuthenticateUserController.ts
+++ src/controllers/AuthenticateUserController.ts
@@ -5,8 +5,12 @@ class AuthenticateUserController {
   async handle(req: Request, res: Response) {
     const {code} = req.body;
     const service = new AuthenticateUserService();
-    const result = await service.execute(code);
-    return res.json(result);
+    try {
+      const result = await service.execute(code);
+      return res.json(result);
+    } catch (error) {
+      return res.json({error: error.message});
+    }
   }
 }
 export {AuthenticateUserController};
